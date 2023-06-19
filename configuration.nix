8f222b87b5eb2f1b881be328ccfc6838676158ef
mistXML <112780421+mistXML@users.noreply.github.com>
2023-06-19 08:46:31 +0000

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,140 @@
+ config, pkgs, lib, ... }:
+{
+
+  nixpkgs.config.allowUnfree = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  imports =
+        [ 
+        ./hardware-configuration.nix
+        ];
+
+	
+  boot = {
+  	loader = {
+        	systemd-boot.enable = true;
+        	efi.canTouchEfiVariables = true;
+		timeout = 0;
+	};
+	initrd.systemd.enable = true;
+  	kernelParams = ["amd_iommu=on" "iommu=pt" "rw"]
+  }; 
+
+  networking = {
+        networkmanager.enable = true;
+        hostName="maelstorm";
+  };
+
+  virtualisation.libvirtd.enable = true; 	
+
+  services = {
+  	dbus.enable = true;
+	xserver.displayManager = {
+  		sddm.enable = true;
+  		defaultSession = "none+Hyprland";
+		autoLogin.enable = true;
+		autoLogin.user = "vael";
+	};
+	pipewire = {
+		enable = true;
+		alsa.enable = true;
+    		alsa.support32Bit = true;
+    		pulse.enable = true;
+    		jack.enable = true;
+	};
+  };
+
+  security.pam.services.gtklock = {};
+
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console.keyMap = "us";
+
+  users = {
+  	defaultUserShell = pkgs.zsh;
+	groups = {
+		vael = {};
+	};
+        users.vael = {
+  		isNormalUser = true;
+		initialPassword = "changeme";
+  		extraGroups = [ "wheel" "libvirt" "kvm" "video" "autologin" "vael"];
+  	};
+	environment.systemPackages = with pkgs; [
+        	git
+		starship
+		dunst
+		fontconfig
+		gimp
+		lutris
+		pavucontrol
+		qemu
+		steam
+		libverto
+		hyprland-protocols
+		hyprland-share-picker
+		xdg-desktop-portal-hyprland
+		hyprpaper
+        	btop
+        	neovim
+        	pulseaudio #Needed for volume keys even with pipewire
+        	python3
+        	unzip
+        	virt-manager
+        	wget
+        	wl-clipboard
+        	zip
+		hyprland
+		freetube
+		bitwarden
+		fuzzel
+		waybar
+		webcord
+		brave
+		firefox
+		imv
+		wezterm
+		mpv
+  };
+
+  programs.steam = {
+	  enable = true;
+	  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+	  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+	  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+    };
+
+  
+  fonts = {
+    fonts = with pkgs; [
+      noto-fonts-cjk-sans
+      fira
+      monocraft
+      fira-code
+      (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
+      roboto
+    ];
+
+    enableDefaultFonts = true;
+
+     fontconfig = {
+      cache32Bit = true;
+      defaultFonts = {
+        sansSerif = ["Monocraft"];
+        monospace = ["Fira Code"];
+    };
+
+
+
+  system.stateVersion = "23.05";
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
+
+
+}
