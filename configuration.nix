21c0a137198d7c25ed41e3e18dc103d8fff1b497
mistXML <112780421+mistXML@users.noreply.github.com>
2023-06-20 14:58:20 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,117 +1,119 @@
-	{config, pkgs, lib, ... }:
+{config, pkgs, lib, ... }:
 
-	{
+{
 
-  		nixpkgs.config.allowUnfree = true;
-  		hardware.opengl.driSupport32Bit = true;
-  		hardware.pulseaudio.support32Bit = true;
+  nixpkgs.config.allowUnfree = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
 
-		imports =
-				[ 
-				./hardware-configuration.nix
-				];
+  imports =
+        [ 
+        ./hardware-configuration.nix
+        ];
 
-  		boot.loader = {
-        	systemd-boot.enable = true;
-        	efi.canTouchEfiVariables = true;
-			timeout = 0;
-			};
+	
+   boot.loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+		timeout = 0;
+	};
 
-  		boot.initrd.systemd.enable = true;
-  		boot.kernelParams = ["amd_iommu=on" "iommu=pt" "rw"];
+  	boot.initrd.systemd.enable = true;
+ 	boot.kernelParams = ["amd_iommu=on" "iommu=pt" "rw"];
 
 
-  		networking.hostName = "maelstorm";
-  		networking.networkmanager.enable = true;
+  	networking.hostName = "maelstorm";
+  	networking.networkmanager.enable = true;
 
-  		virtualisation.libvirtd.enable = true; 	
+  	virtualisation.libvirtd.enable = true; 	
 
   
-		services = { 
-			dbus.enable = true;
-			pipewire = { 
-				enable = true;
-				pipewire.alsa.enable = true;
-				pipewire.alsa.support32Bit = true;
-				pipewire.pulse.enable = true;
-				pipewire.jack.enable = true;
-				};
-			xserver.displayManager = { 
-				sddm.enable = true;
-				defaultSession = "none+Hyprland";
-				server.displayManager.autoLogin.enable = true;
-				server.displayManager.autoLogin.user = "vael";
+  	services = { 
+  		dbus.enable = true;
+  		pipewire = { 
+			enable = true;
+  			pipewire.alsa.enable = true;
+  			pipewire.alsa.support32Bit = true;
+  			pipewire.pulse.enable = true;
+  			pipewire.jack.enable = true;
+    		};
+		xserver.displayManager = { 
+			sddm.enable = true;
+    		defaultSession = "none+Hyprland";
+    		server.displayManager.autoLogin.enable = true;
+    		server.displayManager.autoLogin.user = "vael";
 			};
+	};
 
-			security.pam.services.gtklock = {};
+  security.pam.services.gtklock = {};
 
-			time.timeZone = "Europe/London";
-			i18n.defaultLocale = "en_GB.UTF-8";
-			console.keyMap = "us";
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console.keyMap = "us";
 
-			users = {
-				defaultUserShell = pkgs.zsh;
-				groups = {
-					vael = {};
-				};
-					users.vael = {
-					isNormalUser = true;
-					initialPassword = "changeme";
-					extraGroups = [ "wheel" "libvirt" "kvm" "video" "autologin" "vael"];
-				};
+  users = {
+  	defaultUserShell = pkgs.zsh;
+	groups = {
+		vael = {};
+	};
+        users.vael = {
+  		isNormalUser = true;
+		initialPassword = "changeme";
+  		extraGroups = [ "wheel" "libvirt" "kvm" "video" "autologin" "vael"];
+  	};
 
-				environment.systemPackages = with pkgs; [
-					git
-					starship
-					dunst
-					fontconfig
-					gimp
-					lutris
-					pavucontrol
-					qemu
-					steam
-					libverto
-					hyprland-protocols
-					hyprland-share-picker
-					xdg-desktop-portal-hyprland
-					hyprpaper
-					btop
-					neovim
-					pulseaudio #Needed for volume keys even with pipewire
-					python3
-					unzip
-					virt-manager
-					wget
-					wl-clipboard
-					zip
-					hyprland
-					freetube
-					bitwarden
-					fuzzel
-					waybar
-					webcord
-					brave
-					firefox
-					imv
-					wezterm
-					mpv
-			];
+	environment.systemPackages = with pkgs; [
+        git
+		starship
+		dunst
+		fontconfig
+		gimp
+		lutris
+		pavucontrol
+		qemu
+		steam
+		libverto
+		hyprland-protocols
+		hyprland-share-picker
+		xdg-desktop-portal-hyprland
+		hyprpaper
+        btop
+        neovim
+        pulseaudio #Needed for volume keys even with pipewire
+        python3
+        unzip
+        virt-manager
+        wget
+        wl-clipboard
+        zip
+		hyprland
+		freetube
+		bitwarden
+		fuzzel
+		waybar
+		webcord
+		brave
+		firefox
+		imv
+		wezterm
+		mpv
+  ];
 
-			programs.steam = {
-				enable = true;
-				remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-				dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-				};
+  programs.steam = {
+	  enable = true;
+	  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+	  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  	  };
 
-			xdg.portal = {
-				enable = true;
-				extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-				};
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+    };
 
-			system.stateVersion = "23.05";
-			system.autoUpgrade.enable = true;
-			system.autoUpgrade.allowReboot = true;
-			system.autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
+  system.stateVersion = "23.05";
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-23.05";
 
 
-	}	
+}
